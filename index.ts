46dca713dd4010038491d6eac276c1c1e4afb009
tomjeatt <thomas.jeatt@ovoenergy.com>
2020-07-17 12:03:43 +0100

    Add socket.io

--- index.ts
+++ index.ts
@@ -1,8 +1,18 @@
 import express from 'express';
 import http from 'http';
+import io from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
+const ws = io(server);
+
+ws.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 app.get('/', (_req, res) => {
   res.sendFile(__dirname + '/index.html');
